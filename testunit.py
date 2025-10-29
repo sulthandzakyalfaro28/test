35d0831257aecac04de2b947289d8485d648a952
SulthanDA28 <dzakysultan30@gmail.com>
2025-10-29 11:09:35 +0700

    ubah

--- testunit.py
+++ testunit.py
@@ -1,8 +1,5 @@
-def plus(a,b):
-    return a + b
-def minus(a,b):
-    return a - b
-def fib_recursive(n: int) -> int:
-    if n <= 1:
-        return n
-    return fib_recursive(n - 1) + fib_recursive(n - 2)
+class Calculator:
+    def add(self, a, b):
+        return a + b
+    def subtract(self, a, b):
+        return a - b
